87fe27e36e24fcddea8630f069310a7d21802ddd
irsatsaridas <109540989+irsatsaridas@users.noreply.github.com>
2022-10-20 19:35:08 +0300

    Variable nmemb switched with count

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,9 +16,9 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	ptr = malloc(nmemb * size);
+	ptr = malloc(count * size);
 	if (ptr == NULL)
 		return (NULL);
-	ft_bzero(ptr, nmemb);
+	ft_bzero(ptr, count);
 	return (ptr);
 }
